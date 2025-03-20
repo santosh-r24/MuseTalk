565c3b7b610d6d3656d8e62a156beb725a6b1c49
Santosh R <santoshramakrishnan24@gmail.com>
2025-03-20 17:12:31 +0530

    modifying musetalk scripts
    Modifying utils to update path as per directory structure in colab, restoring realtime_inference.py as it was

--- musetalk/utils/utils.py
+++ musetalk/utils/utils.py
@@ -15,11 +15,13 @@ from musetalk.whisper.audio2feature import Audio2Feature
 from musetalk.models.vae import VAE
 from musetalk.models.unet import UNet,PositionalEncoding
 
+MODEL_DIR = "/content/drive/MyDrive/VegetaAvatar/MuseTalk"
+
 def load_all_model():
-    audio_processor = Audio2Feature(model_path="./models/whisper/tiny.pt")
-    vae = VAE(model_path = "./models/sd-vae-ft-mse/")
-    unet = UNet(unet_config="./models/musetalk/musetalk.json",
-                model_path ="./models/musetalk/pytorch_model.bin")
+    audio_processor = Audio2Feature(model_path=f"{MODEL_DIR}/models/whisper/tiny.pt")
+    vae = VAE(model_path = f"{MODEL_DIR}/models/sd-vae-ft-mse/")
+    unet = UNet(unet_config=f"{MODEL_DIR}/models/musetalk/musetalk.json",
+                model_path =f"{MODEL_DIR}/models/musetalk/pytorch_model.bin")
     pe = PositionalEncoding(d_model=384)
     return audio_processor,vae,unet,pe
 
