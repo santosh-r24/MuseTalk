e7d7e1dcea7b9ea59aee3c0f5acae8fed196c1cf
Santosh R <santoshramakrishnan24@gmail.com>
2025-09-18 17:03:46 +0530

    Update preprocessing.py
    To use runpod endpoint

--- musetalk/utils/preprocessing.py
+++ musetalk/utils/preprocessing.py
@@ -14,8 +14,8 @@ from tqdm import tqdm
 
 # initialize the mmpose model
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-config_file = '/workspace/MuseTalk/musetalk/utils/dwpose/rtmpose-l_8xb32-270e_coco-ubody-wholebody-384x288.py'
-checkpoint_file = '/workspace/MuseTalk/models/dwpose/dw-ll_ucoco_384.pth'
+config_file = '/runpod-volume/MuseTalk/musetalk/utils/dwpose/rtmpose-l_8xb32-270e_coco-ubody-wholebody-384x288.py'
+checkpoint_file = '/runpod-volume/MuseTalk/models/dwpose/dw-ll_ucoco_384.pth'
 model = init_model(config_file, checkpoint_file, device=device)
 
 # initialize the face detection model
